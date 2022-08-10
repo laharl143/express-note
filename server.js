22e5307d6247e89800936ac4deedde89e37215c7
Erskine Gamaliel L. Duenas <91664149+laharl143@users.noreply.github.com>
2022-08-10 17:09:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,31 @@
 // CTRL + C = to exit the node; 
 // $ nodemon server.js = is used for the automation; 
 
+// How to set this step by step:
+// Make a new folder called my-express-server on your Desktop
+
+// Change Directory to this new folder
+
+// Inside the my-express-server folder, create a new file called server.js
+
+// Set up a new NPM package ($ npm init)
+
+// Open the project folder in vscode
+
+// Using NPM install the express module ($ npm install express)
+
+// Require express in your server.js   (const express = require('express'))
+
+// Setup express  (const app = express())
+
+// Create a root route get method with app.get()
+
+// Send the words Hello World from the root route as the response
+
+// Spin up our server on port 3000 with app.listen
+
+// Run server with nodemon  ($ nodemon server.js)
+
 const express = require('express')
 const app = express()
 const port = 3000
